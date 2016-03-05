8200095e206be19d117c4e72734ae10f87c2a682
ajibolaayanwola <ajibolaayanwola@yahoo.com>
2016-03-04 18:10:08 -0600

    updated ads 4

--- js/myfunctions.js
+++ js/myfunctions.js
@@ -71,9 +71,10 @@ function adSetter() {
 	}
 
 	if (typeof AdMob !== "undefined") {
+		alert("screen.width: " + screen.width "\n$(window).width(): " + $(window).width());
 		AdMob.createBanner({
 			adId : admobid.banner,
-			adSize : "CUSTOM", width : 360, height : 50,
+			adSize : "CUSTOM", width : screen.width, height : 50,
 			overlap : true,
 			position : AdMob.AD_POSITION.TOP_CENTER,
 			autoShow : true
